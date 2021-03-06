0f6aa3dff83de051f77704b3f5102ad4fb65135f
Andres Fajardo <andresf.f321@yahoo.comgit config user.emailgit config --global user.email andresf.f321@yahoo.com>
2021-03-05 19:27:51 -0800

    finished the readme generator

---
+++ README.md
@@ -0,0 +1,44 @@
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]
+
+# README generator
+
+## Description
+
+the project can generate README's using node.js
+
+
+|      [Install](#Install)      |    [Usage](#Usage)      |
+| ------------------------------|-------------------------|
+| [Contribution](#Contribution) |   [Testing](#Testing)   |
+| ------------------------------|-------------------------|
+|     [License](#License)       | [Questions](#Questions) |
+
+
+## Install
+
+    to install simply just clone the repository or download zip and extract, then open the folder with git bash and type 'node index.js'
+
+
+## Usage
+
+    type 'node index.js'
+
+
+## Contribution
+
+    you can contribute by making a new branch and changing the code in the branch to whatever you think is better
+
+
+## Testing
+
+    you can test the code by typing 'node index.js' with git bash
+
+
+## License
+
+MIT
+
+## Questions
+
+if you have any questions you can contact me [here](https://github.com/AFF321) or email me [here](mailto:andresf.f321@yahoo.com)
--- index.js
+++ index.js
@@ -2,6 +2,56 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
+const generateMD = (answers) =>
+
+
+`
+${answers.license2}
+
+# ${answers.Title}
+
+## Description
+
+${answers.describe}
+
+
+|      [Install](#Install)      |    [Usage](#Usage)      |
+| ------------------------------|-------------------------|
+| [Contribution](#Contribution) |   [Testing](#Testing)   |
+| ------------------------------|-------------------------|
+|     [License](#License)       | [Questions](#Questions) |
+
+
+## Install
+
+    ${answers.install}
+
+
+## Usage
+
+    ${answers.usage}
+
+
+## Contribution
+
+    ${answers.contribution}
+
+
+## Testing
+
+    ${answers.test}
+
+
+## License
+
+${answers.license}
+
+## Questions
+
+if you have any questions you can contact me [here](${answers.user}) or email me [here](mailto:${answers.email})
+`
+
+
 inquirer
     .prompt([
         {
@@ -11,7 +61,7 @@ inquirer
         },
         {
             type:'input',
-            name: 'link',
+            name: 'describe',
             message:'what is your project about?',
         },
         {
@@ -26,7 +76,7 @@ inquirer
         },
         {
             type:'input',
-            name: 'contrabution',
+            name: 'contribution',
             message:'how can they contribute?',
         },
         {
@@ -38,12 +88,18 @@ inquirer
             type:'checkbox',
             name: 'license',
             message:'what license?',
-            choices:['MIT','Apache','GNU general public']
+            choices:['MIT','Apache','GNU']
+        },
+        {
+            type:'checkbox',
+            name: 'license2',
+            message:'what license?',
+            choices:['[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]','[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]','[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]']
         },
         {
             type:'input',
             name: 'user',
-            message:'what is your GitHub username?',
+            message:'what is your GitHub link?',
         },
         {
             type:'input',
@@ -51,12 +107,13 @@ inquirer
             message:'your email address?',
         },
     ])
+    .then((answers) => {
+        console.log('nextstep')
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+        const README = generateMD(answers);
 
-// TODO: Create a function to initialize app
-function init() {}
+        fs.writeFile ('README.md',README, (err) =>
+        err ? console.log(err) : console.log("Readme successfully made!")
+        );
+    })
 
-// Function call to initialize app
-init();
